4d034cbf4f519502cfeab4f0d56eb126a82a0923
Sana Gaikwad <gaikwadsana@gmail.com>
2024-12-01 18:58:44 +1100

    chore: configure cors on vercel

--- src/api/festivals.ts
+++ src/api/festivals.ts
@@ -1,6 +1,6 @@
 import { Festival } from "../types"
 
-const FESTIVALS_API_BASE_URL = "https://eacp.energyaustralia.com.au/codingtest/api/v1/festivals"
+const FESTIVALS_API_BASE_URL = "/api/festivals"
 
 export const fetchFestivals = async (): Promise<Festival[]> => {
   const response = await fetch(FESTIVALS_API_BASE_URL)
